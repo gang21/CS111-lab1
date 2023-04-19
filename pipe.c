1dbdf42c97649e8d2524b7284cbc507fcc363a52
Gabriella Ang <gabriella90631@gmail.com>
2023-04-19 15:43:40 -0700

    bug fix

--- pipe.c
+++ pipe.c
@@ -9,7 +9,6 @@ int main(int argc, char *argv[])
 	//creating the pipe and checking for errors
 	int pipefd[2];
 	pid_t cpid;
-	pipe(pipefd);
 	if(argc < 2) {
 		exit(EXIT_FAILURE);
 	}
@@ -20,13 +19,6 @@ int main(int argc, char *argv[])
 	printf("ARGV[2]: %s\n", argv[2]);
 	//forking the process
 	cpid = fork();
-
-	//checking for errors in the fork
-	if(cpid == fork()) {
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-
 	//switching child process to next argument
 	if (cpid == 0) {
 		printf("child process ran");
@@ -34,9 +26,13 @@ int main(int argc, char *argv[])
 	}
 
 	//running the parent process
-	if (cpid > 0) {
+	else if (cpid > 0) {
 		printf("parent process ran");
 		execlp(argv[1], argv[1], NULL);
+	}
+
+	else {
+		perror("fork");
 	}
 
 
