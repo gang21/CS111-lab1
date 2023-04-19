33be29a9aa78d5285e1a693c4f57e9723b4d6214
Gabriella Ang <gabriella90631@gmail.com>
2023-04-19 10:14:47 -0700

    fd example

--- pipe.c
+++ pipe.c
@@ -4,16 +4,35 @@
 
 int main(int argc, char *argv[])
 {
-	int return_code = fork();
-  if (return_code == 0) {
-    printf("This is the child process!\n");
-    execlp("ls", "ls", NULL);
-  } else if (return_code > 0) {
-    printf("I am lazy parent, letting my child to ls the directory\n");
-    printf("I will just wait for their report\n");
-  } else {
-    printf("Child process creation error! \n");
-  }
-  printf("They finished; Done!\n");
-  return 0;
+	int pipefd[2];
+	pid_t cpid;
+	char buf;
+	if (argc != 2) {
+		exit(-1);
+	}
+	if (pipe(pipefd) == -1) {
+		perror("pipe");
+		exit(-1);
+	}
+	cpid = fork();
+	if (cpid == -1) {
+		perror("fork");
+		exit(-1);
+	}
+	if (cpid == 0) {
+		close(pipefd[1]);
+		while(read(pipefd[0], &buf, 1) > 0) {
+			write(STDOUT_FILENO, &buf, 1);
+		}
+		write(STDOUT_FILENO, "\n", 1);
+		close(pipefd[0]);
+		exit(0);
+	}
+	else {
+		close(pipefd[0]);
+		write(pipefd[1], argv[1], strlen(argv[1]));
+		close(pipefd[1]);
+		wait(NULL);
+		exit(0);
+	}
 }
