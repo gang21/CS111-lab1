40651edb93039fe7f61e7203e2ba4cee4be66e3c
Gabriella Ang <gabriella90631@gmail.com>
2023-04-19 13:29:00 -0700

    dup2 example

--- pipe.c
+++ pipe.c
@@ -1,38 +1,14 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[])
 {
-	int pipefd[2];
-	pid_t cpid;
-	char buf;
-	if (argc != 2) {
-		exit(-1);
-	}
-	if (pipe(pipefd) == -1) {
-		perror("pipe");
-		exit(-1);
-	}
-	cpid = fork();
-	if (cpid == -1) {
-		perror("fork");
-		exit(-1);
-	}
-	if (cpid == 0) {
-		close(pipefd[1]);
-		while(read(pipefd[0], &buf, 1) > 0) {
-			write(STDOUT_FILENO, &buf, 1);
-		}
-		write(STDOUT_FILENO, "\n", 1);
-		close(pipefd[0]);
-		exit(0);
-	}
-	else {
-		close(pipefd[0]);
-		write(pipefd[1], argv[1], strlen(argv[1]));
-		close(pipefd[1]);
-		wait(NULL);
-		exit(0);
-	}
+	int fd = open("my_new_file.txt", O_APPEND|O_WRONLY);
+	if (fd < 0)
+		printf("Error opening the file\n");
+	dup2(fd, STDOUT_FILENO);
+	printf("print to the my_new_file.txt, instead of the console \n");
+	return 0;
 }
