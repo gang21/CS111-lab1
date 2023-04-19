11201ac8f3eea8aefa6af810ee40a327009ceced
Gabriella Ang <gabriella90631@gmail.com>
2023-04-19 13:34:28 -0700

    printing out arguments

--- pipe.c
+++ pipe.c
@@ -5,10 +5,6 @@
 
 int main(int argc, char *argv[])
 {
-	int fd = open("my_new_file.txt", O_APPEND|O_WRONLY);
-	if (fd < 0)
-		printf("Error opening the file\n");
-	dup2(fd, STDOUT_FILENO);
-	printf("print to the my_new_file.txt, instead of the console \n");
+	print(argv);
 	return 0;
 }
