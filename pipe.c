44581ec6ac0d3b358d661fe7bafac3e147dd650c
Gabriella Ang <gabriella90631@gmail.com>
2023-04-19 15:45:01 -0700

    bug fix

--- pipe.c
+++ pipe.c
@@ -22,13 +22,13 @@ int main(int argc, char *argv[])
 	//switching child process to next argument
 	if (cpid == 0) {
 		printf("child process ran");
-		execlp(argv[2], argv[2], NULL);
+		// execlp(argv[2], argv[2], NULL);
 	}
 
 	//running the parent process
 	else if (cpid > 0) {
 		printf("parent process ran");
-		execlp(argv[1], argv[1], NULL);
+		// execlp(argv[1], argv[1], NULL);
 	}
 
 	else {
