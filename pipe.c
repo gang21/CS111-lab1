f4f10436d2f6915a0d086b4948f9756d4552a66b
Gabriella Ang <gabriella90631@gmail.com>
2023-04-19 14:49:17 -0700

    pipe tests

--- pipe.c
+++ pipe.c
@@ -2,12 +2,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-	printf("argc: %d\n", argc);
-	for (int i = 0; i < argc; i++) {
-		printf("argv[%d]: %s\n",i, argv[i]);
+	//checking for errors in the creation of the pipe/fork
+	int pipefd[2];
+	pid_t cpid;
+	char buf;
+	if(argc != 2) {
+		exit(EXIT_FAILURE);
 	}
+	if (pipe(pipefd) == -1) {
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+	cpid = fork();
+	if(cpid = fork()) {
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+
+	execlp(argv[1], argv[1], NULL);
+
+
+
 	return 0;
 }
