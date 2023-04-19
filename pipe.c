9ef914926dac02d94a65419fe2d93d00b9738d77
Gabriella Ang <gabriella90631@gmail.com>
2023-04-19 13:38:41 -0700

    adding newline

--- pipe.c
+++ pipe.c
@@ -5,9 +5,9 @@
 
 int main(int argc, char *argv[])
 {
-	printf("argc: %d", argc);
+	printf("argc: %d\n", argc);
 	for (int i = 0; i < argc; i++) {
-		printf("argv[%d]: %s",i, argv[i]);
+		printf("argv[%d]: %s\n",i, argv[i]);
 	}
 	return 0;
 }
