97264b05a0850e07b52fdebdd3f92c367d616854
Gabriella Ang <gabriella90631@gmail.com>
2023-04-21 16:25:13 -0700

    bug fixes

--- pipe.c
+++ pipe.c
@@ -8,38 +8,78 @@
 
 int main(int argc, char *argv[])
 {
-	//pipe creating
-	int fd[2];
-	if (pipe(fd) == -1) {
-		return(EXIT_FAILURE);
-	}
-	int cpid = fork();
-	if (cpid < 0) {
-		return(EXIT_FAILURE);
-	}
-	//child process
-	if(cpid == 0) {
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		execlp(argv[1], argv[1], NULL);
+	// //pipe creating
+	// int fd[2];
+	// if (pipe(fd) == -1) {
+	// 	return(EXIT_FAILURE);
+	// }
+	// int cpid = fork();
+	// if (cpid < 0) {
+	// 	return(EXIT_FAILURE);
+	// }
+	// //child process
+	// if(cpid == 0) {
+	// 	dup2(fd[1], STDOUT_FILENO);
+	// 	close(fd[0]);
+	// 	close(fd[1]);
+	// 	execlp(argv[1], argv[1], NULL);
 
+	// }
+	// //parent
+	// int ppid = fork();
+	// if (ppid < 0) {
+	// 	return(EXIT_FAILURE);
+	// }
+	// if(ppid == 0) {
+	// 	dup2(fd[0], STDIN_FILENO);
+	// 	close(fd[0]);
+	// 	close(fd[1]);
+	// 	execlp(argv[2], argv[2], NULL);
+	// }
+	// close(fd[0]);
+	// close(fd[1]);
+	// waitpid(cpid, NULL, 0);
+	// waitpid(ppid, NULL, 0);
+
+
+	////////////////////////////////////////////////
+	/////////       for loop example      //////////
+	////////////////////////////////////////////////
+
+	int pids[argc - 1];
+	int pipes[argc];
+	int i;
+	for (i = 0; i < argc - 1; i++) {
+		if (pipe(pipes[i]) == -1) {
+			printf("Error with creating pipe\n");
+			return(EXIT_FAILURE);
+		}
 	}
-	//parent
-	int ppid = fork();
-	if (ppid < 0) {
-		return(EXIT_FAILURE);
+
+	for (i = 0; i < argc - 1; i++) {
+		pids[i] = fork();
+		if(pids[i] == -1) {
+			printf("Error with creating processes\n");
+			return(EXIT_FAILURE);
+		}
+		if(pids[i] == 0) {
+			//child process
+
+			int j;
+			for(j = 0; j < argc; j++) {
+				if(i != j) {
+					close(pipes[j][0]);
+				}
+				if(i+1 != j) {
+					close(pipes[j][1]);
+				}
+			}
+			return(EXIT_SUCCESS);
+		}
 	}
-	if(ppid == 0) {
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		execlp(argv[2], argv[2], NULL);
+	for (i=0; i < argc - 1; i++) {
+		wait(NULL);
 	}
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(cpid, NULL, 0);
-	waitpid(ppid, NULL, 0);
 
 	return 0;
 }
