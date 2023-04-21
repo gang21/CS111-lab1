e2bf85d2e5e988dd118c361d6c23197e0e7ac922
Gabriella Ang <gabriella90631@gmail.com>
2023-04-21 12:09:33 -0700

    added waitpid() in parent

--- pipe.c
+++ pipe.c
@@ -6,17 +6,10 @@
 
 int main(int argc, char *argv[])
 {
-	// //creating the pipe and checking for errors
-	// int pipefd[2];
 	pid_t cpid;
-	// if(argc < 2) {
-	// 	exit(EXIT_FAILURE);
-	// }
-	// if (pipe(pipefd) == -1) {
-	// 	perror("pipe");
-	// 	exit(EXIT_FAILURE);
-	// }
-
+	if(argc < 2) {
+		exit(EXIT_FAILURE);
+	}
 	//forking the process
 	cpid = fork();
 
@@ -24,11 +17,13 @@ int main(int argc, char *argv[])
 		int fd[2];
 		if (pipe(fd) < 0) {
 			perror("pipe");
+			exit(EXIT_FAILURE);
 		}
 		pid_t child = fork();
 		if(child == 0) {
 			if(dup2(fd[1], 1) < 0) {
 				perror("dup2");
+				exit(EXIT_FAILURE);
 			}
 			printf("Process 1 Running\n");
 			close(fd[0]);
@@ -38,7 +33,11 @@ int main(int argc, char *argv[])
 		else {
 			if(dup2(fd[0], 0) < 0) {
 				perror("dup2");
+				exit(EXIT_FAILURE);
 			}
+
+			int status = 0;
+			waitpid(cpid, &status, 0);
 			printf("Process 2 Running\n");
 			close(fd[0]);
 			execlp(argv[2], argv[2], NULL);
