7b671168905049f60ebf8b09ea3136d5ae3f2077
Gabriella Ang <gabriella90631@gmail.com>
2023-04-22 10:56:29 -0700

    bug fixes

--- pipe.c
+++ pipe.c
@@ -79,7 +79,7 @@ int main(int argc, char *argv[])
 	if (pids[0] == 0) {
 		printf("First Process: %s\n", argv[1]);
 		dup2(pipes[1][1], STDOUT_FILENO);
-		close(STDOUT_FILENO);
+		// close(STDOUT_FILENO);
 		execlp(argv[1], argv[1], NULL);
 	}
 	
