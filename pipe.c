90531cb84cf8b00779ce2a048d23175ccdf34f46
Gabriella Ang <gabriella90631@gmail.com>
2023-04-22 14:44:59 -0700

    bug fixes

--- pipe.c
+++ pipe.c
@@ -167,8 +167,8 @@ int main(int argc, char *argv[])
 	}
 
 	// //only do last process if there is a 2nd argument
-	printf("argc: %d", argc);
-	if(argc > 2) {
+	printf("argc: %d\n", argc);
+	if(argc != 2) {
 		//last process
 		pids[PROCESS_NUM] = fork();
 		if (pids[PROCESS_NUM] == 0) {
