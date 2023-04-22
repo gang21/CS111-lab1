8e69181cb5d5253d0b99a9e8908399b637a7349c
Gabriella Ang <gabriella90631@gmail.com>
2023-04-22 16:38:23 -0700

    bug fixes

--- pipe.c
+++ pipe.c
@@ -42,19 +42,17 @@ int main(int argc, char *argv[])
 	}
 
 	for(i = 1; i < PROCESS_NUM; i++) {
-		printf("Process %d: %s\n", i+1, argv[i+1]);
 		pids[i+1] = fork();
 		if(pids[i+1] < 0) {
 			return(EXIT_FAILURE);
 		}
 		if(pids[i+1] == 0) {
-			printf("Process (inner) %d: %s\n", i+1, argv[i+1]);
-			dup2(pipes[i][0], STDIN_FILENO);
-			dup2(pipes[i+1][1], STDOUT_FILENO);
+			dup2(pipes[i-1][0], STDIN_FILENO);
+			dup2(pipes[i][1], STDOUT_FILENO);
+			close(pipes[i-1][0]);
+			close(pipes[i-1][1]);
 			close(pipes[i][0]);
 			close(pipes[i][1]);
-			close(pipes[i+1][0]);
-			close(pipes[i+1][1]);
 			execlp(argv[i+1], argv[i+1], NULL);
 		}
 		close(pipes[i][0]);
