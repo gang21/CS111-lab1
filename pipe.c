44c695ad1c333f4f464734c3ef1eb1dfc9e4a536
Gabriella Ang <gabriella90631@gmail.com>
2023-04-22 09:48:51 -0700

    bug fixes

--- pipe.c
+++ pipe.c
@@ -46,49 +46,75 @@ int main(int argc, char *argv[])
 	/////////       for loop example      //////////
 	////////////////////////////////////////////////
 
-	int pipes[argc][2];
-	int pids[argc - 1];
+	int PROCESS_NUM = argc - 1;
+	int pipes[PROCESS_NUM + 1][2];
+	int pids[PROCESS_NUM];
 	int i;
-	for (i = 0; i < argc; i++) {
-		if (pipe(pipes[i]) == -1) {
-			printf("Error with creating pipe\n");
-			return(EXIT_FAILURE);
+
+	//creating the pipes
+	for(i = 0; i < PROCESS_NUM + 1; i++) {
+		if(pipe(pipes[i]) == -1) {
+			printf("error with creating pipe\n");
+			exit(EXIT_FAILURE);
 		}
 	}
 
-	for (i = 1; i < argc; i++) {
-		//creating forks
+	//creating actual processes
+	for(i = 0; i < PROCESS_NUM; i++) {
 		pids[i] = fork();
 		if(pids[i] == -1) {
 			printf("Error with creating processes\n");
-			return(EXIT_FAILURE);
+			exit(EXIT_FAILURE);
 		}
-		if(pids[i] == 0) {
-			//child process
+		//child process
+		if (pids[i] == 0) {
+			//closing unused pipes
 			int j;
-			for(j = 0; j < argc; j++) {
-				if(i != j) {
+			for(j = 0; j < PROCESS_NUM + 1; j++) {
+				if (i != j) {
 					close(pipes[j][0]);
 				}
 				if(i+1 != j) {
 					close(pipes[j][1]);
 				}
 			}
-			
+
+			int x;
+			if(read(pipes[i][0], &x, sizeof(int)) == -1) {
+				printf("Error at reading\n");
+				exit(EXIT_FAILURE);
+			}
+
+
+			printf("(%d) Got %d\n", i,x);
+			x++;
+			if(write(pipes[i+1][1], &x, sizeof(int)) == -1) {
+				printf("Error at writing\n");
+				exit(EXIT_FAILURE);
+			}
+			printf("(%d) Sent %d\n", i,x);
+
 			close(pipes[i][0]);
 			close(pipes[i+1][1]);
-
-			//if execlp doesn't work
-			// return(EXIT_SUCCESS);
+			return 0;
 		}
-		
-		//main process
-		if(i < argc - 1) {
-			printf("This is loop #%d with p1: %s and p2: %s\n", i, argv[i], argv[i+1]);
-		}		
 	}
 
-	for (i=0; i < argc - 1; i++) {
+	//main process
+	int y = 5;
+	printf("Main process sent %d\n", y);
+	if(write(pipes[0][1], &y, sizeof(int)) == -1) {
+		printf("Error at writing\n");
+		exit(EXIT_FAILURE);
+	}
+	if(read(pipes[PROCESS_NUM][0], &y, sizeof(int)) == -1) {
+		printf("Error at reading\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("The final result is %d\n", y);
+
+	//wait for all child processes to finish execution
+	for(i = 0; i < PROCESS_NUM; i++) {
 		wait(NULL);
 	}
 
