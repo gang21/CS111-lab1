442e45b0ba6f625af80313d81da5267d826134ec
Gabriella Ang <gabriella90631@gmail.com>
2023-04-22 16:27:38 -0700

    bug fixes

--- pipe.c
+++ pipe.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 
 	}
 
-	for(i = 1; i < PROCESS_NUM-1; i++) {
+	for(i = 1; i < PROCESS_NUM; i++) {
 		pids[i+1] = fork();
 		if(pids[i] < 0) {
 			return(EXIT_FAILURE);
