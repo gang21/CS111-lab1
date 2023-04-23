2835e3539393a72f24fca533a60c59e168ef480c
Gabriella Ang <gabriella90631@gmail.com>
2023-04-22 20:49:28 -0700

    bug fixes

--- pipe.c
+++ pipe.c
@@ -57,27 +57,27 @@ int main(int argc, char *argv[])
 		}
 
 		//closing unused pipes
-		int j;
-		for(j = 0; j < PROCESS_NUM + 1; j++) {
-			if (i != j) {
-				close(pipes[j][0]);
-			}
-			if(i+1 != j) {
-				close(pipes[j][1]);
-			}
-		}
+		// int j;
+		// for(j = 0; j < PROCESS_NUM + 1; j++) {
+		// 	if (i != j) {
+		// 		close(pipes[j][0]);
+		// 	}
+		// 	if(i+1 != j) {
+		// 		close(pipes[j][1]);
+		// 	}
+		// }
 		if(pids[i] == 0) {
 			printf("Process (inner) %d: %s\n", i+1, argv[i+1]);
-			dup2(pipes[i][0], STDIN_FILENO);
-			dup2(pipes[i+1][1], STDOUT_FILENO);
+			dup2(pipes[i-1][0], STDIN_FILENO);
+			dup2(pipes[i][1], STDOUT_FILENO);
+			close(pipes[i-1][0]);
+			close(pipes[i-1][1]);
 			close(pipes[i][0]);
 			close(pipes[i][1]);
-			close(pipes[i+1][0]);
-			close(pipes[i+1][1]);
 			execlp(argv[i+1], argv[i+1], NULL);
 		}
-		close(pipes[i][0]);
-		close(pipes[i][1]);
+		close(pipes[i-1][0]);
+		close(pipes[i-1][1]);
 	}
 	//parent
 	// int ppid = fork();
